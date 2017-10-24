fd81657b2163aa5e054eb0187eb07e3c223a10ec
root <root@domain.tld>
2017-10-24 07:17:55 +0000

    useless 1164

--- iTrust/fuzzer/useless.py
+++ iTrust/fuzzer/useless.py
@@ -5,7 +5,8 @@ buildslist = []
 
 def useless(buildslist):
     logdict = {}
-    #buildslist = [112,113,114,115,116,117,118,119,120,121]
+    testdict = {}
+    #buildslist = [126,127,128,129,130]
     listlen = len(buildslist)
     for i in buildslist:
         with open('/var/lib/jenkins/jobs/itrust_test2/builds/'+str(i)+'/log', 'r') as logfile:
@@ -15,16 +16,18 @@ def useless(buildslist):
         print "Length of logs"+str(i)+" : "+str(loglen)
         j = 0
         while (j<loglen):
-            test = re.search("Tests run: \d+, Failures: 0,.*- in (.*)\n",logdata_list[j])
+            test = re.search("Tests run: (\d+), Failures: 0,.*- in (.*)\n",logdata_list[j])
 	    if test:
     	        #print test.group(1)
-		if (logdict.has_key(test.group(1))):
-		    temp = logdict.get(test.group(1))
+		if (logdict.has_key(test.group(2))):
+		    temp = logdict.get(test.group(2))
                     if temp:
                         temp+=1
-                        logdict.update({test.group(1):temp})
+                        logdict.update({test.group(2):temp})
+			testdict.update({test.group(2):int(test.group(1))})
 	        else:
-	     	    logdict.update({test.group(1):1})
+	     	    logdict.update({test.group(2):1})
+		    testdict.update({test.group(2):int(test.group(1))})
                     #print logdict	
 	    j+=1
         #i+=1
@@ -32,14 +35,18 @@ def useless(buildslist):
     logtestlist = logdict.keys()
     vallen = len(values)
     uselesstests = []
+    uselesscount = 0
     for i in range(vallen):
 	if values[i]==listlen:
 	    uselesstests.append(logtestlist[i])
+	    uselesscount+=testdict[logtestlist[i]]
 
     logfile.close()
+    #print "Length of Dictionary: "+str(len(logdict))
     print uselesstests
-    print "Number of useless tests: "+str(len(uselesstests))
+    print "Number of useless tests: "+str(uselesscount)
     print "Exited Useless test detector!"
+
 
 
 #useless(buildslist)
